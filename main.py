661833daf434e170279aa992f438b7c2ffad0cdc
ELIFNUR ONDER <elifnuronder@ELIFNUR-MacBook-Pro.local>
2020-11-30 20:02:59 +0300

    Change function name, delete unneccessary docs

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
 import string
 
 letters = list(string.ascii_lowercase)
-name = 'kuuuzey'
 
 zlist = []
 for a in range(len(letters)):
     zlist = [0] * (a + 1)
 
-def bok(x, y):
-    # zlist = []
-    # for a in range(len(letters)):
-    #     zlist = [0] * (a + 1)
+def my_func(x, y):
+
 
     for j in x:
         for i in letters:
@@ -18,7 +15,7 @@ def bok(x, y):
                 y[letters.index(i)] +=1
     return y
 
-print(bok("bokasdgfhjkldAsdfghjnhgfdsdfvgbn", zlist ))
+print(my_func("deneme", zlist ))
 
 
 
