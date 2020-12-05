3c66b6e4b17006a38e2a129cab5aa2413762dd4b
ELIFNUR ONDER <elifnuronder@ELIFNUR-MacBook-Pro.local>
2020-12-05 20:08:22 +0300

    52 numbers are distrubted to 4 arrays

---
+++ exercise_2.py
@@ -0,0 +1,16 @@
+import random
+
+# numbs = list(range(1,53))
+# dict = { 'A' :[], 'B':[], 'C': [], 'D': []}
+#
+# for i in range(int(len(numbs)/len(dict))):
+#
+#     for j in dict:
+#
+#         a= random.choice(numbs)
+#         dict [j] . append (a)
+#         b=numbs.index(a)
+#         del numbs[b]
+#
+# print(dict)
+
